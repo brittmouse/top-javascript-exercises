11d4a8bc313dacf6fc3abe3246ea82b88b04aa8c
brittmouse <bstancoff@proton.me>
2023-10-29 11:17:11 -0400

    tempConversion solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  const c = (5 / 9) * (f - 32);
+  return Math.round(c * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  const f = (9 * c / 5) + 32;
+  return Math.round(f * 10) / 10;
 };
 
 // Do not edit below this line
