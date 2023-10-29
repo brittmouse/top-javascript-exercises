ef3337122e3e6f43134345c424c9ab44f4390e1e
brittmouse <bstancoff@proton.me>
2023-10-29 10:33:40 -0400

    sumAll solved
    While the solution file uses a `for` loop, I decided to try a
    `do while` loop to see if it would work, and it does.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (
+    (a < 0 || b < 0) ||
+    (typeof (a) !== 'number' || typeof (b) !== 'number')
+  ) return "ERROR";
 
+  const max = Math.max(a, b);
+  let min = Math.min(a, b);
+  let sum = 0;
+
+  do {
+    sum += min;
+    min++;
+  } while (min <= max);
+  return sum;
 };
 
 // Do not edit below this line
