723b4bdc56caca4cb478c88d105c26e3eaee907a
brittmouse <bstancoff@proton.me>
2023-10-28 22:21:08 -0400

    reverseString solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(oldString) {
+  let newString = "";
+  const strLen = oldString.length;
+  if (strLen > 0) {
+    for (let i = 1; i <= strLen; i++) {
+      newString += oldString[strLen - i];
+    }
+  }
+  return newString;
 };
 
 // Do not edit below this line
