0f26e269f78b68e7a2e2f927a335ae08d3b1f1f4
brittmouse <bstancoff@proton.me>
2023-11-08 12:23:14 -0500

    fibonacci solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num < 0) return "OOPS";
+  if (num < 2) return 1;
+  let fibSequence = [1, 1];
+  for (let i = 2; i < num; i++) {
+    let fibLen = fibSequence.length;
+    let a = fibSequence[fibLen - 2];
+    let b = fibSequence[fibLen - 1];
+    let c = a + b;
+    fibSequence[i] = c;
+  }
+  return fibSequence[fibSequence.length - 1];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
