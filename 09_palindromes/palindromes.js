d8b828f48c2ebbd2146f005be8340505eb4dbf60
brittmouse <bstancoff@proton.me>
2023-11-07 21:51:54 -0500

    palindrome solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+  let newStr = "";
+  const letters = 'qwertyiuopasdfghjklzxcvbnm';
+  const digits = '1234567890';
+  str = str.toLowerCase();
+  for (chr in str) {
+    if (letters.includes(str[chr]) || digits.includes(str[chr])) newStr += str[chr];
+  }
+  return newStr === newStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
