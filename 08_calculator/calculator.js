0d1f96249bb0ed266c0848e4ab58d925db385deb
brittmouse <bstancoff@proton.me>
2023-11-07 20:21:14 -0500

    calculator solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numList) {
+  let sum = 0;
+  for (let i = 0; i < numList.length; i++) {
+    sum += numList[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numList) {
+  let product = 1;
+  for (let i = 0; i < numList.length; i++) {
+    product *= numList[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let answer = 1;
+  if (a >= 2) {
+    let factor = 2;
+    while (factor <= a) {
+      answer *= factor;
+      factor++;
+    }
+  }
+  return answer;
 };
 
 // Do not edit below this line
